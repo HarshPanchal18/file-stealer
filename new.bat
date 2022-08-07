8d737503b13e991d043fb1c7c1b42f08a35984b1
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-08-07 12:00:42 +0530

    Add files via upload

---
+++ new.bat
@@ -0,0 +1,9 @@
+@echo off
+:: variables
+/min
+SET odrive=%odrive:~0,2%
+set backupcmd=xcopy /s /c /d /e /h /i /r /y
+echo off
+%backupcmd% "C:\Users\%USERPROFILE%\LetslearnGit" "%drive%allMy pics"
+@echo off
+cls
